962e196d2695ff7a995db9ca89bc2a787cd5266f
Hatemmamdoh <36468246+Hatemmamdoh@users.noreply.github.com>
2019-12-21 23:50:39 +0200

    First

--- src/Project.java
+++ src/Project.java
@@ -2,9 +2,9 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class Project {
-	 String name;
+	 String name ,startDay;
 	 int cost;
-	 Date StartingDate, DueDates , startDay;
+	 Date StartingDate, DueDates ;
 	 int hoursPerDay, ActualProjectHours;
 	 ArrayList<Task> tasks;
 	
@@ -12,7 +12,7 @@ public class Project {
 		 tasks = new ArrayList<Task>();
 	 }
 	 
-	 public Project ( String name, int cost, Date StartingDate, Date DueDates , Date startDay, int hoursPerDay, int ActualProjectHourst) {
+	 public Project ( String name, int cost, Date StartingDate, Date DueDates , String startDay, int hoursPerDay, int ActualProjectHourst) {
 		this.name = name;
 		this.cost = cost;
 		this.StartingDate = StartingDate;
@@ -30,8 +30,29 @@ public class Project {
 		
 	}
 	public Project load(String name) {
-		//
-		//
+		String Projectname = null; //"SELECT Name FROM project WHERE name = name " ;
+		int cost = 0; // SELECT cost FROM project WHERE name = name "
+		Date Start = null ; // SELECT startingday FROM project WHERE name = name "
+		Date DueDates = null ; // SELECT DueDates FROM project WHERE name = name "
+		String StartDay = null ; // SELECT StartDay FROM project WHERE name = name "
+		int hoursPerDay = 0 ;// // SELECT StartDay FROM project WHERE name = name "
+		int ActualProjectHours = 0 ;// // SELECT StartDay FROM project WHERE name = name "
+		
+		
+		this.name = Projectname;
+		this.cost = cost;
+		this.StartingDate = Start;
+		this.DueDates = DueDates;
+		this.startDay = StartDay;
+		this.hoursPerDay = hoursPerDay;
+		this.ActualProjectHours = ActualProjectHours;
+		
 		return this;
 	}
+	public void add ()
+	{
+		//INSERT INTO Project (name, cost,StartingDate ,DueDates ,startDay,hoursPerDay,ActualProjectHours,)
+		//VALUES (this.name, this.cost, this.StartingDate, this.DueDates,this.startDay,this.hoursPerDay,this.ActualProjectHours,);
+	}
+	
 }
--- src/Task.java
+++ src/Task.java
@@ -1,5 +1,8 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Vector;
 
 public class Task {
 	//TaskID, ID(project),
@@ -27,6 +30,34 @@ public class Task {
 	}
 	public void loadFromDatabase() {
 		
+	}
+	public Vector<Task> load(String Projectname)
+	{
+		Vector<Task> v = null ;
+		String SQL = "jdbc:sqlserver://LAPTOP-M7JGO3P2:1433;databaseName=Library;integratedSecurity=true";
+		int projectID = 0 ;////"SELECT ID FROM project WHERE name = Projectname "
+	    /*	
+        String sql = "SELECT* FROM Task INNER JOIN Project ON Task.projectID = Project.projectID; "
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection connection = DriverManager.getConnection(SQL);
+        Statement statement = connection.createStatement();
+        PreparedStatement pst = connection.prepareStatement(sql);
+        ResultSet rs = pst.executeQuery(sql);
+		*/
+		
+		/*
+		 while (rs.next()) {   
+		    Task t = new Task ;   
+		    Task.Taskname = rs.getString("Taskname") ;
+		    Task.workinghours = rs.getString("workinghours") ;
+		    Task.delivaerable = rs.getString("workinghours") ;
+		    Task.startDate = rs.getString("workinghours") ;
+		    Task.DueDate = rs.getString("workinghours") ;
+		    v.add(t) ;
+		                                   
+		}
+		*/
+		return v;
 	}
 
 }
