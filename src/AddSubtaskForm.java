1206dd7bfabd3abacbbb1cc1e17a9bec89943270
Khaled ElHadary <khaledelhadary@outlook.com>
2019-12-22 16:41:00 +0200

    Subtask Form  - 1

---
+++ src/AddSubtaskForm.java
@@ -0,0 +1,119 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+public class AddSubtaskForm {
+
+	private JFrame frame;
+	private JTextField Subtask_TF;
+	private JTextField preID_TF;
+	private JTextField workingHours_TF;
+	private JTextField startDate_TF;
+	private JTextField dueDate_TF;
+	private JTextField deliverable_TF;
+	private JTextField TaskID_TF;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AddSubtaskForm window = new AddSubtaskForm();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public AddSubtaskForm() {
+		initialize();
+		frame.setVisible(true);
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 368);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblSubtaskName = new JLabel("Subtask name");
+		lblSubtaskName.setBounds(59, 90, 100, 14);
+		frame.getContentPane().add(lblSubtaskName);
+		
+		Subtask_TF = new JTextField();
+		Subtask_TF.setColumns(10);
+		Subtask_TF.setBounds(169, 87, 197, 20);
+		frame.getContentPane().add(Subtask_TF);
+		
+		preID_TF = new JTextField();
+		preID_TF.setColumns(10);
+		preID_TF.setBounds(169, 118, 197, 20);
+		frame.getContentPane().add(preID_TF);
+		
+		JLabel label_1 = new JLabel("Predecessor ID");
+		label_1.setBounds(59, 121, 100, 14);
+		frame.getContentPane().add(label_1);
+		
+		JLabel label_2 = new JLabel("Working Hours");
+		label_2.setBounds(59, 149, 100, 14);
+		frame.getContentPane().add(label_2);
+		
+		workingHours_TF = new JTextField();
+		workingHours_TF.setColumns(10);
+		workingHours_TF.setBounds(169, 146, 197, 20);
+		frame.getContentPane().add(workingHours_TF);
+		
+		JLabel label_3 = new JLabel("Start Date");
+		label_3.setBounds(59, 177, 100, 14);
+		frame.getContentPane().add(label_3);
+		
+		startDate_TF = new JTextField();
+		startDate_TF.setColumns(10);
+		startDate_TF.setBounds(169, 174, 197, 20);
+		frame.getContentPane().add(startDate_TF);
+		
+		JLabel label_4 = new JLabel("Due Date");
+		label_4.setBounds(59, 205, 100, 14);
+		frame.getContentPane().add(label_4);
+		
+		dueDate_TF = new JTextField();
+		dueDate_TF.setColumns(10);
+		dueDate_TF.setBounds(169, 202, 197, 20);
+		frame.getContentPane().add(dueDate_TF);
+		
+		deliverable_TF = new JTextField();
+		deliverable_TF.setColumns(10);
+		deliverable_TF.setBounds(169, 230, 197, 20);
+		frame.getContentPane().add(deliverable_TF);
+		
+		JLabel label_5 = new JLabel("Deliverable");
+		label_5.setBounds(59, 233, 100, 14);
+		frame.getContentPane().add(label_5);
+		
+		JButton submitBtn = new JButton("Submit");
+		submitBtn.setBounds(164, 280, 89, 23);
+		frame.getContentPane().add(submitBtn);
+		
+		JLabel lblTaskId = new JLabel("Task ID");
+		lblTaskId.setBounds(59, 62, 100, 14);
+		frame.getContentPane().add(lblTaskId);
+		
+		TaskID_TF = new JTextField();
+		TaskID_TF.setColumns(10);
+		TaskID_TF.setBounds(169, 59, 197, 20);
+		frame.getContentPane().add(TaskID_TF);
+	}
+}
--- src/MainFrame.java
+++ src/MainFrame.java
@@ -29,7 +29,6 @@ public class MainFrame {
 	private DefaultTableModel model1 ;
 	private DefaultTableModel model2 ;
 	private JTextField textField;
-	private JTextField textField_1;
 	private JTextField textField_2;
 	private JButton btnNewButton_3;
 	private JButton btnNewButton_4;
@@ -65,7 +64,7 @@ public class MainFrame {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 1083, 648);
+		frame.setBounds(100, 100, 957, 576);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 
@@ -75,7 +74,7 @@ public class MainFrame {
 		
 		
 		JButton btnNewButton = new JButton("Show Members");
-		btnNewButton.setBounds(538, 473, 105, 23);
+		btnNewButton.setBounds(627, 463, 127, 23);
 		frame.getContentPane().add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("Add Task");
@@ -85,59 +84,56 @@ public class MainFrame {
 				frame.setVisible(false);
 			}
 		});
-		btnNewButton_1.setBounds(922, 473, 135, 23);
+		btnNewButton_1.setBounds(146, 463, 170, 23);
 		frame.getContentPane().add(btnNewButton_1);
-		
-		JButton btnNewButton_2 = new JButton("Show Subtasks");
-		btnNewButton_2.setBounds(112, 473, 105, 23);
-		frame.getContentPane().add(btnNewButton_2);
 		
 		
 		
 		JLabel lblNewLabel = new JLabel("Tasks");
-		lblNewLabel.setBounds(286, 22, 46, 14);
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel.setBounds(286, 22, 69, 14);
 		frame.getContentPane().add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Members");
-		lblNewLabel_1.setBounds(848, 22, 46, 14);
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel_1.setBounds(739, 25, 182, 14);
 		frame.getContentPane().add(lblNewLabel_1);
 		
 		textField = new JTextField();
-		textField.setBounds(650, 474, 86, 20);
+		textField.setBounds(765, 464, 143, 20);
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
 		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(227, 474, 105, 20);
-		frame.getContentPane().add(textField_1);
-		
 		JButton btnAddSubtask = new JButton("Add subtask");
-		btnAddSubtask.setBounds(922, 507, 135, 23);
+		btnAddSubtask.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnAddSubtask.setBounds(31, 463, 105, 23);
 		frame.getContentPane().add(btnAddSubtask);
 		
-		JButton btnAddProjectMembers = new JButton("Add Project Member");
-		btnAddProjectMembers.setBounds(776, 575, 135, 23);
+		JButton btnAddProjectMembers = new JButton("Add Member");
+		btnAddProjectMembers.setBounds(627, 497, 127, 23);
 		frame.getContentPane().add(btnAddProjectMembers);
 		
 		textField_2 = new JTextField();
-		textField_2.setBounds(922, 576, 135, 20);
+		textField_2.setBounds(765, 498, 143, 20);
 		frame.getContentPane().add(textField_2);
 		textField_2.setColumns(10);
 		
 		btnNewButton_3 = new JButton("Show Chart");
-		btnNewButton_3.setBounds(112, 575, 105, 23);
+		btnNewButton_3.setBounds(31, 497, 105, 23);
 		frame.getContentPane().add(btnNewButton_3);
 		
 		btnNewButton_4 = new JButton("Assign Task Members");
-		btnNewButton_4.setBounds(453, 575, 142, 23);
+		btnNewButton_4.setBounds(146, 497, 170, 23);
 		frame.getContentPane().add(btnNewButton_4);
 		
 		
 	}
 
 	private void initializeTables() {
-		table1 = new JTable(new DefaultTableModel(new Object[]{"Predecessor ID" , "Task ID",  "Name", "Working Hours", "StartDate", "DueDates" , "deliverable"},0));
+		table1 = new JTable(new DefaultTableModel(new Object[]{"Predecessor ID" , "Task ID",  "Name", "Working Hours", "StartDate", "DueDate" , "deliverable"},0));
 		table1.setBounds(500, 28, 292, 392);
 		table1.setFont(new Font("Consolas", Font.BOLD, 14));
 		table1.setFillsViewportHeight(true);
@@ -148,6 +144,10 @@ public class MainFrame {
 		table1.setIntercellSpacing(new Dimension(40,5));
 		table1.setRowHeight(30);
 		model = (DefaultTableModel) table1.getModel();
+		
+		table1.getColumnModel().getColumn(0).setMinWidth(100);
+		table1.getColumnModel().getColumn(1).setMaxWidth(60);
+		table1.getColumnModel().getColumn(3).setMinWidth(100);
 		
 		scrollPane1 = new JScrollPane(table1);
 		scrollPane1.setBounds(31, 47, 571, 396);
@@ -164,7 +164,7 @@ public class MainFrame {
 		model2 = (DefaultTableModel) table2.getModel();
 		
 		scrollPane2 = new JScrollPane(table2);
-		scrollPane2.setBounds(690, 47, 367, 396);
+		scrollPane2.setBounds(627, 47, 281, 396);
 		scrollPane2.setPreferredSize(new Dimension(100, 226));
 		
 	}
